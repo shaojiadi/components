2da8eb9b9cb4976eaee0eaa1794b79032685e53d
邵佳迪 <1923943838qq.com>
2021-03-05 13:51:12 +0800

    新建项目

---
+++ components/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <Table />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Table from '@/components/Table.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Table
+  }
+}
+</script>
